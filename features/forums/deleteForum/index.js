64cfa1107e2f335b50365e5a8b9a583eefce5fc2
JDgomez2002 <87883400+JDgomez2002@users.noreply.github.com>
2025-10-09 09:05:51 -0600

    fix: just creator and admin can delete forums

--- features/forums/deleteForum/index.js
+++ features/forums/deleteForum/index.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import { ForumModel } from "./schema.js";
+import { ForumModel, UserModel } from "./schema.js";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -10,12 +10,15 @@ if (!uri) {
   throw new Error("URI not found in the environment");
 }
 
-export const handler = async (event, context) => {
+export const handler = async (event) => {
   try {
     await mongoose.connect(uri);
 
     // Get the forum ID from query parameters or path parameters
     const forumId = event.pathParameters?.id || event.queryStringParameters?.id;
+
+    // Extract user_id from the Lambda authorizer context
+    const userId = event.requestContext?.authorizer?.lambda?.user_id;
 
     // Validate that ID is provided
     if (!forumId) {
@@ -33,6 +36,42 @@ export const handler = async (event, context) => {
         statusCode: 400,
         body: JSON.stringify({
           message: "Invalid MongoDB ObjectId forum ID format",
+        }),
+      };
+    }
+
+    const user = await UserModel.findOne({ clerk_id: userId });
+
+    if (!user) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({
+          message: "User not found",
+        }),
+      };
+    }
+
+    const forum = await ForumModel.findById(forumId);
+
+    if (!forum) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({
+          message: "Forum not found",
+        }),
+      };
+    }
+
+    // verify that the user is the creator of the forum, just admins and the creator can delete the forum
+    if (
+      user._id.toString() !== forum.creator_id.toString() &&
+      user.role !== "admin"
+    ) {
+      return {
+        statusCode: 403,
+        body: JSON.stringify({
+          message:
+            "Forbidden: Only admins and the creator can delete the forum",
         }),
       };
     }
