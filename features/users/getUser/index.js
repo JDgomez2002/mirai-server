b2e3563ed4dddeda4d8d15b243665615499f52f8
JDgomez2002 <87883400+JDgomez2002@users.noreply.github.com>
2025-10-27 22:39:52 -0600

    feat: handle user pii encryption

--- features/users/getUser/index.js
+++ features/users/getUser/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import { decrypt } from "./crypto.utils.js";
 import { encrypt } from "./traffic.crypto.js";
 
+const encryptPIIData = true;
+
 dotenv.config();
 
 const uri = process.env.URI;
@@ -53,13 +55,13 @@ export const handler = async (event, _) => {
       statusCode: 200,
       body: JSON.stringify({
         user: {
-          _id: encrypt(user._id.toString()),
-          first_name: encrypt(decrypt(user.first_name)),
-          last_name: encrypt(decrypt(user.last_name)),
-          image_url: encrypt(decrypt(user.image_url)),
-          username: encrypt(decrypt(user.username)),
-          email: encrypt(decrypt(user.email)),
-          role: encrypt(user.role),
+          _id: user._id,
+          first_name: handleEncryption(user.first_name, encryptPIIData),
+          last_name: handleEncryption(user.last_name, encryptPIIData),
+          image_url: handleEncryption(user.image_url, encryptPIIData),
+          username: handleEncryption(user.username, encryptPIIData),
+          email: handleEncryption(user.email, encryptPIIData),
+          role: handleEncryption(user.role, encryptPIIData),
           tags: user.user_tags,
           quizCompletedAt: user.quizCompletedAt ?? null,
           likes: user.likes ?? [],
@@ -74,4 +76,18 @@ export const handler = async (event, _) => {
       }),
     };
   }
+};
+
+const handleEncryption = (value, encryptPIIData) => {
+  if (!value) {
+    return null;
+  }
+
+  const parts = value.split(":");
+  if (parts.length !== 2) {
+    return encryptPIIData ? encrypt(value) : value;
+  }
+
+  const decryptedValue = decrypt(value);
+  return encryptPIIData ? encrypt(decryptedValue) : decryptedValue;
 };
